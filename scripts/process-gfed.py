84f2c6baab4a57c2ff78a2540b31108164fcb6c7
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2018-06-27 18:58:42 +0200

    Convert GFED4s data to Tg

--- scripts/process-gfed.py
+++ scripts/process-gfed.py
@@ -12,10 +12,19 @@ out = {}
 for name in files:
     path = root / "raw_data" / name
     with open(path) as f:
+        # Read the 4th row with unit information
+        for _ in range(3):
+            f.readline()
+        unit = f.readline()
+        factor = int(
+            unit.split("estimates in ")[1].split(" g ")[0].split("E")[1]
+        )
+        f.seek(0)
         data = pd.read_table(f, skiprows=8, delim_whitespace=True, nrows=15)
         data = data.set_index("Region").T.loc['1997':'2018']
         data.index = [int(i) for i in data.index]
         data = data.Global
+        data = data / 10**(12 - factor)  # to Tg
         # Parse gas name from e.g. 'GFED4.1s_SO2.txt'
         idx = path.suffixes[0].split("_")[1]
         data.name = idx
