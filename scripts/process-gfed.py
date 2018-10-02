6329720ed6042bad43af561704d20b7ed2e4b694
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2018-10-02 14:41:41 +0200

    Limit GFED4S to complete year (2017) and calculate NMVOC

--- scripts/process-gfed.py
+++ scripts/process-gfed.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
+from pandas_datapackage_reader import read_datapackage
 from pathlib import Path
+from pandas.util.testing import assert_almost_equal
 
 root = Path(__file__).parents[1]
 
@@ -21,15 +23,32 @@ for name in files:
         )
         f.seek(0)
         data = pd.read_table(f, skiprows=8, delim_whitespace=True, nrows=15)
-        data = data.set_index("Region").T.loc['1997':'2018']
+        data = data.set_index("Region").T.loc['1997':'2017']
         data.index = [int(i) for i in data.index]
         data = data.Global
         data = data / 10**(12 - factor)  # to Tg
-        # Parse gas name from e.g. 'GFED4.1s_SO2.txt'
-        idx = path.suffixes[0].split("_")[1]
+        # Parse gas name from e.g. 'GFED4.1s_SO2.txt',
+        # or 'GFED4.1s_Toluene_lump.txt'
+        idx = path.suffixes[0].split("_", 1)[1]
         data.name = idx
         out[idx] = data
 
+# From http://www.globalfiredata.org/ar6historic.html with NH3 removed
+# as it is already contained in the data separately.
+nmvoc = ["C2H6", "CH3OH", "C2H5OH", "C3H8", "C2H2", "C2H4", "C3H6", "C5H8", "C10H16", "C7H8", "C6H6", "C8H10", "Toluene_lump", "Higher_Alkenes", "Higher_Alkanes", "CH2O", "C2H4O", "C3H6O", "C2H6S", "HCN", "HCOOH", "CH3COOH", "MEK", "CH3COCHO", "HOCH2CHO"
+]
+
 df = pd.DataFrame(out)
 df.index.name = "Year"
+df["NMVOC"] = df[nmvoc].sum(axis=1)
+df.index = pd.PeriodIndex(df.index, freq="A")
+gbbe = read_datapackage(root / "datapackage.json", "global-biomass-burning-emissions")
+
+# Test for sufficient equality of NMVOC sum with GBBE data
+# where it is already combined.
+assert_almost_equal(
+    gbbe.NMVOC.loc['1997':'2015'].round(0),
+    df["NMVOC"].loc['1997':'2015'].round(0)
+)
+
 df.to_csv(root / "data/gfed4s.csv")
